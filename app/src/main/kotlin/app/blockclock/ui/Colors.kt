59977ce4a1577fbeb581599a7b0528011b8524e8
atomofiron <atomofiron@gmail.com>
2026-08-27 04:29:38 +0300

    move colors into Colors.kt

--- app/src/main/kotlin/app/blockclock/ui/Colors.kt
+++ app/src/main/kotlin/app/blockclock/ui/Colors.kt
@@ -3,6 +3,14 @@ package app.blockclock.ui
 import androidx.compose.ui.graphics.Color
 
 object Colors {
+    val Purple80 = Color(0xFFD0BCFF)
+    val PurpleGrey80 = Color(0xFFCCC2DC)
+    val Pink80 = Color(0xFFEFB8C8)
+
+    val Purple40 = Color(0xFF6650a4)
+    val PurpleGrey40 = Color(0xFF625b71)
+    val Pink40 = Color(0xFF7D5260)
+
     val SwatchBorder = Color(0x33000000)
     val MarkerBorder = Color(0xff808080)
 }
--- app/src/main/kotlin/app/blockclock/ui/theme/Theme.kt
+++ app/src/main/kotlin/app/blockclock/ui/theme/Theme.kt
@@ -10,23 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import app.blockclock.ui.Colors
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Colors.Purple80,
+    secondary = Colors.PurpleGrey80,
+    tertiary = Colors.Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Colors.Purple40,
+    secondary = Colors.PurpleGrey40,
+    tertiary = Colors.Pink40
 )
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         SDK_INT >= AndroidS -> {
