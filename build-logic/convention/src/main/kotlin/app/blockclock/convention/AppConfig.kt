86dcc33f90793be9f6595514a5bfa082d110044b
atomofiron <atomofiron@gmail.com>
2026-09-24 22:49:43 +0200

    up app version to 1.2.1 (4)

--- build-logic/convention/src/main/kotlin/app/blockclock/convention/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/app/blockclock/convention/AppConfig.kt
@@ -13,6 +13,6 @@ object AppConfig {
     const val compileSdk = 37
     const val compileSdkMinor = 1
 
-    const val versionCode = 3
-    const val versionName = "1.2.0"
+    const val versionCode = 4
+    const val versionName = "1.2.1"
 }
